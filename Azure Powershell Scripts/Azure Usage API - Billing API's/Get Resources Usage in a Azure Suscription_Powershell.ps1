0f2da3827469e0251880fca9c30338848674f941
The Azure Guy <theazureguy007@gmail.com>
2017-07-07 14:31:18 +0530

    Azure Usage Billing API ARM Script
    Get Resources Usage in a Azure Suscription_Powershell

---
+++ Azure Powershell Scripts/Azure Usage API - Billing API's/Get Resources Usage in a Azure Suscription_Powershell.ps1
@@ -0,0 +1,15 @@
+﻿Get-Module -ListAvailable -Name AzureRm.Resources | Select Version
+
+Login-AzureRmAccount
+
+Get-AzureRmSubscription
+
+Set-AzureRmContext -SubscriptionName "Visual Studio Dev Essentials"
+
+// Gives out the Detailed Usages or actions or resources consumed in Your Azure Account
+Get-UsageAggregates -ReportedStartTime "5/7/2017" -ReportedEndTime "7/7/2017"
+
+
+
+
+Get-ModuleList
