9a425c97e93468721dd80b08f782c75a755e3f74
The Azure Guy <theazureguy007@gmail.com>
2017-07-09 01:38:08 +0530

    Deploy a Virtual Machine using an ARM Template

---
+++ Azure Powershell Scripts/Deploy a Virtual Machine using an ARM Template/Deploy a Virtual Machine using an ARM Template.ps1
@@ -0,0 +1,8 @@
+﻿
+Login-AzureRmAccount
+
+// Create Resource Group
+New-AzureRmResourceGroup -Name "TheAzureGuyRG" -Location "Central India"
+
+New-AzureRmResourceGroupDeployment -Name ExampleDeployment -ResourceGroupName TheAzureGuyRG
+  -TemplateFile c:\MyTemplates\storage.json -storageAccountType Standard_GRS
