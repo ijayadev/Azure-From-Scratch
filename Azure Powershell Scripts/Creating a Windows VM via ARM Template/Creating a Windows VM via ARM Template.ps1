ecd522d280ad8e2d572f34c61d15852df0a1ab11
The Azure Guy <theazureguy007@gmail.com>
2017-07-09 00:38:54 +0530

    ARM Template & Powershell for Windows VM creation
    Creating a Windows VM via ARM Template & Powershell

---
+++ Azure Powershell Scripts/Creating a Windows VM via ARM Template/Creating a Windows VM via ARM Template.ps1
@@ -0,0 +1,8 @@
+﻿// Available Locations
+Get-AzureRmLocation | sort DisplayName | Select DisplayName
+
+// Create Resource Group
+New-AzureRmResourceGroup -Name "TheAzureGuyRG" -Location "Central India"
+
+// DNS Name should Alpha numeric and lower case
+New-AzureRmResourceGroupDeployment -ResourceGroupName "TheAzureGuyRG" -TemplateUri "https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-vm-simple-windows/azuredeploy.json"
