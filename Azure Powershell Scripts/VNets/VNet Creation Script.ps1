626fa35deb7deefdcd72827061970aa28fc5be25
The Azure Guy <theazureguy007@gmail.com>
2017-07-25 15:44:21 +0530

    VNet Creation Script.ps1

---
+++ Azure Powershell Scripts/VNets/VNet Creation Script.ps1
@@ -0,0 +1,7 @@
+﻿Add-AzureRmAccount
+
+New-AzureRmResourceGroup -Name TestRG -Location centralus
+New-AzureRmVirtualNetwork -ResourceGroupName TestRG -Name TestVNet -AddressPrefix 192.168.0.0/16 -Location centralus
+$vnet = Get-AzureRmVirtualNetwork -ResourceGroupName TestRG -Name TestVNet
+Add-AzureRmVirtualNetworkSubnetConfig -Name FrontEnd -VirtualNetwork $vnet -AddressPrefix 192.168.1.0/24
+Add-AzureRmVirtualNetworkSubnetConfig -Name BackEnd -VirtualNetwork $vnet -AddressPrefix 192.168.2.0/24
